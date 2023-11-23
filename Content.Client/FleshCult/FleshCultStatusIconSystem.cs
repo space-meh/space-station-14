5c3ad74306848d5da3602c2bdb032e79a1cba108
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2023-11-23 19:15:36 +1000

    Added showing flesh cultist icon

---
+++ Content.Client/FleshCult/FleshCultStatusIconSystem.cs
@@ -0,0 +1,21 @@
+using Content.Client.Antag;
+using Content.Shared.StatusIcon.Components;
+using Content.Shared.FleshCult;
+
+namespace Content.Client.FleshCult;
+
+public sealed class FleshCultStatusIconSystem : AntagStatusIconSystem<FleshCultistComponent>
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<FleshCultistComponent, GetStatusIconsEvent>(GetIcon);
+    }
+
+    // TODO: Does the ability to see the icons of other cultists become available from changing mutations?
+    private void GetIcon(Entity<FleshCultistComponent> fleshCult, ref GetStatusIconsEvent args)
+    {
+        GetStatusIcon(fleshCult.Comp.FactionPrototypeId, ref args);
+    }
+}
